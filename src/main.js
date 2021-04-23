be4d5932e1c498bfd6584c1ca6361f666d73cbe8
bulakh <bulakh94@gmail.com>
2021-04-23 15:10:25 +0300

    Добавляет новый презентер для хедера

--- src/main.js
+++ src/main.js
@@ -1,33 +1,21 @@
-import NavigationView from './view/navigation.js';
-import FiltersView from './view/filters.js';
-import InformationRoutView from './view/information-rout.js';
 import {generateWaypoint} from './mock/waypoint.js';
-import {generateInformationOfRout, generateEmptyInformation} from './mock/information-rout.js';
+import TripHeaderPresenter from './presenter/trip-header.js';
 import TripPesenter from './presenter/trip.js';
-import {render, RenderPosition} from './utils/render.js';
 
-const WAYPOINT_COUNT = 6;
+const WAYPOINT_COUNT = 22;
 const EDIT_FORM = true;
-let informationOfRout;
-
 
 const waypoints = new Array(WAYPOINT_COUNT).fill().map(generateWaypoint);
-if (waypoints.length === 0) {
-  informationOfRout = generateEmptyInformation();
-} else {
-  informationOfRout = generateInformationOfRout(waypoints);
-}
 
 const tripMainElement = document.querySelector('.trip-main');
 const tripNavigationElement = tripMainElement.querySelector('.trip-controls__navigation');
 const tripFiltersElement = tripMainElement.querySelector('.trip-controls__filters');
-render(tripNavigationElement, new NavigationView(), RenderPosition.BEFOREEND);
-render(tripFiltersElement, new FiltersView(), RenderPosition.BEFOREEND);
-render(tripMainElement, new InformationRoutView(informationOfRout), RenderPosition.AFTERBEGIN);
-
-
 const tripEventsElement = document.querySelector('.trip-events');
-const tripPresenter = new TripPesenter(tripEventsElement);
 
+const tripHeaderPresenter = new TripHeaderPresenter(tripMainElement, tripNavigationElement, tripFiltersElement);
+tripHeaderPresenter.init(waypoints);
+
+
+const tripPresenter = new TripPesenter(tripEventsElement);
 tripPresenter.init(waypoints, EDIT_FORM);
 
